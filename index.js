5d4af77bdbf78da8d41ba68930714fdb2c0ab2ac
Lakhan Nad <lakhannad1999@gmail.com>
2020-06-29 16:22:37 +0530

    Major changes

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.set("x-powered-by", false);
+app.set("view engine", "ejs");
+app.set("views", path.join(process.cwd(), "views"));
+
+app.get("/:static", (req, res, next) => {
+  res.render(req.params.static);
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log("Server started");
+});
